8434ab6144df3ab6d5fa9093a05a5565dbb405bc
Wang Guan <momocraft@gmail.com>
2024-05-31 01:55:24 +0900

    make rustc happy

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,11 +1,11 @@
 use regex::Regex;
 use std::path::{Path, PathBuf};
 
-#[derive(Clone, Copy)]
-pub struct Snapshot<'a> {
-    pub _snapshots_dir: &'a Path,
-    pub volume: &'a str,
-    pub timestamp: &'a str,
+#[derive(Clone)]
+pub struct Snapshot {
+    pub _snapshots_dir: PathBuf,
+    pub volume: String,
+    pub timestamp: String,
     pub gen: Option<u32>,
 }
 
@@ -19,25 +19,23 @@ impl Snapshot {
         */
         let subvol_pattern: Regex =
             Regex::new(r"^(<volume>.*).(<timestamp>[T\d+]{8,})(<gen>_\d+)?$").unwrap();
-        let _snapshots_dir= buffer.parent()?.clone();
+        let _snapshots_dir = buffer.parent()?;
         let last_component = buffer.components().last()?;
         let last_component = last_component.as_os_str().to_str()?;
         let matched = subvol_pattern.captures(last_component)?;
         let volume = matched.name("volume")?.as_str();
         let timestamp = &matched.name("timestamp")?.as_str();
-        let gen = &matched
+        let gen = matched
             .name("gen")
-            .map(|m| m.as_str().parse::<u32>().unwrap());
+            .map(|m| m.as_str().parse::<u32>().unwrap())
+            .clone();
 
         Some(Snapshot {
-            _snapshots_dir,
-            volume: volume.clone(),
-            timestamp: timestamp.clone(),
-            gen: gen.clone(),
+            _snapshots_dir: _snapshots_dir.to_path_buf(),
+            volume: volume.to_string(),
+            timestamp: timestamp.to_string(),
+            gen,
         })
-    }
-    pub fn from_string() -> Option<Snapshot> {
-        todo!()
     }
 }
 
